c66cd22d289bbbbcb0ea9deae38e4fca1a88e2bd
lixinyi405 <1936456448@qq.com>
2026-07-13 21:22:26 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# ===== Coze 配置 =====
+
+COZE_TOKEN = ""
+
+DATASET_ID = ""
+
+BASE_URL = "https://api.coze.cn/open_api"
+
+
+# ===== 微博配置 =====
+
+WEIBO_UID = "5598574734"
+
+# 单依纯UID
+
+
+# ===== 输出 =====
+
+OUTPUT_FILE = "weibo_update.txt"
